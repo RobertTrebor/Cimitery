529e01165823db18e5812c49eaf4bf972697cbc7
Stephanie <stephanie.gerlich@gmail.com>
2013-11-04 14:49:46 +0100

    1st commit database create, no database yet

--- Cimitery/gen/de/cimitery/android/cimitery/R.java
+++ Cimitery/gen/de/cimitery/android/cimitery/R.java
@@ -8,6 +8,9 @@
 package de.cimitery.android.cimitery;
 
 public final class R {
+    public static final class array {
+        public static final int create=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,25 +20,26 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f040003;
+        public static final int app_name=0x7f040002;
+        public static final int database=0x7f040000;
+        public static final int version=0x7f040001;
     }
     public static final class style {
         /** 
@@ -48,10 +52,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
