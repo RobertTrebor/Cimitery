7790f21639c2b2c36b7f6266087e9b823c7999e4
Stephanie <stephanie.gerlich@gmail.com>
2013-11-07 08:56:42 +0100

    zusammengeführt mit Robert, donnerstagmorgen, exif drin

--- Cimitery/src/de/cimitery/android/cimitery/NewGraveActivity.java
+++ Cimitery/src/de/cimitery/android/cimitery/NewGraveActivity.java
@@ -14,7 +14,13 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.MediaColumns;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,8 +29,10 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class NewGraveActivity extends Activity{
 
@@ -33,6 +41,18 @@ public class NewGraveActivity extends Activity{
 	RadioButton radioButton;
 	
 	Grave grave = new Grave();
+	
+	ImageView imageViewTombstone;
+	Bitmap yourSelectedImage;
+	private static final int SELECT_PHOTO = 100;
+	private static final int SELECT_PICTURE = 100;
+	String selectedImagePath;
+	String filemanagerstring;
+	String imagePath;
+	Cursor cursor;
+	int column_index;
+	TextView tv_TombstonePath;
+	ExifData exif;
 
 	
 	@Override
@@ -64,9 +84,19 @@ public class NewGraveActivity extends Activity{
 		});
 		
 		
-		
-		Button selectPhoto = (Button) findViewById(R.id.buttonSelectPhoto);
+		Button buttonSelectPhoto = (Button) findViewById(R.id.buttonSelectPhoto);
+		buttonSelectPhoto.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+						photoPickerIntent.setType("image/*");
+						startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+					}
+
+				});
 		Button button = (Button) findViewById(R.id.buttonNewGrave);
+		
 		button.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -119,6 +149,106 @@ public class NewGraveActivity extends Activity{
 			}
 		});
 	}
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent imageReturnedIntent) {
+		super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+		Log.d("requestCode: " + requestCode, "resultCode: " + resultCode);
+		/*
+		 * switch(requestCode) { case SELECT_PHOTO: if(resultCode == RESULT_OK){
+		 * Uri selectedImage = imageReturnedIntent.getData(); InputStream
+		 * imageStream = null; try { imageStream =
+		 * getContentResolver().openInputStream(selectedImage); } catch
+		 * (FileNotFoundException e) { e.printStackTrace(); } yourSelectedImage
+		 * = BitmapFactory.decodeStream(imageStream); } }
+		 * 
+		 * imageViewTombstone = (ImageView)
+		 * findViewById(R.id.imageViewTombstone);
+		 * imageViewTombstone.setImageBitmap(yourSelectedImage);
+		 */
+		Log.d("DEBUG", "BEFORE IF");
+		Log.d("resultCode", ((Integer) resultCode).toString());
+		if (resultCode == Activity.RESULT_OK) {
+			Log.d("RequestCode", ((Integer) requestCode).toString());
+			if (requestCode == SELECT_PICTURE) {
+				Log.d("DEBUG", "requestCode == SELECT_PICTURE");
+
+				Uri selectedImageUri = imageReturnedIntent.getData();
+
+				// OI FILE Manager
+				filemanagerstring = selectedImageUri.getPath();
+				Log.d("filemanagerstring", selectedImageUri.getPath());
+				// MEDIA GALLERY
+				selectedImagePath = getPath(selectedImageUri);
+				Log.d("selectedImagePath", getPath(selectedImageUri));
+
+				// imageViewTombstone.setImageURI(selectedImageUri);
+
+				imagePath.getBytes();
+				Log.d("DEBUG", "imagePath.getBytes();");
+				TextView tv_TombstonePath = (TextView) findViewById(R.id.tv_TombstonePath);
+				tv_TombstonePath.setText(selectedImagePath.toString());
+
+				// Bitmap bm = BitmapFactory.decodeFile(imagePath);
+
+				ExifInterface exifInterface;
+
+				try {
+					exifInterface = new ExifInterface(selectedImagePath);
+
+					String date = exifInterface
+							.getAttribute(exifInterface.TAG_DATETIME);
+					String width = exifInterface
+							.getAttribute(exifInterface.TAG_IMAGE_WIDTH);
+
+					String LATITUDE = exifInterface
+							.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+					String LATITUDE_REF = exifInterface
+							.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+					String LONGITUDE = exifInterface
+							.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+					String LONGITUDE_REF = exifInterface
+							.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+
+					exif = new ExifData(LATITUDE, LATITUDE_REF, LONGITUDE,
+							LONGITUDE_REF);
+
+					exif.convertFormat();
+					float latitude = exif.getLatitude();
+					System.out.println(latitude);
+					double lat = (double) latitude;
+					
+					System.out.println(lat);
+					
+					
+					
+					float longitude = exif.getLongitude();
+					System.out.println(longitude);
+					double lng = (double) longitude;
+					System.out.println(lng);
+
+					Log.d("CONVERTED", String.valueOf(latitude));
+					Log.d("CONVERTED", String.valueOf(longitude));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public String getPath(Uri uri) {
+		String[] projection = { MediaColumns.DATA };
+		Cursor cursor = managedQuery(uri, projection, null, null, null);
+		column_index = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
+		cursor.moveToFirst();
+		imagePath = cursor.getString(column_index);
+
+		return cursor.getString(column_index);
+	}
+
 
 
 	protected void setAllGraveData() {
