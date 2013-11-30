6d4b988932d6f60f36d18a99ca5706af71bc4c5e
steph789 <stephanie.gerlich@gmail.com>
2013-11-30 17:31:11 +0100

    prepared to get started

--- Cimitery/src/de/cimitery/android/cimitery/CategoriesListAdapter.java
+++ Cimitery/src/de/cimitery/android/cimitery/CategoriesListAdapter.java
@@ -1,70 +1,113 @@
 package de.cimitery.android.cimitery;
 
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
-public class CategoriesListAdapter extends BaseExpandableListAdapter{
+public class CategoriesListAdapter extends BaseExpandableListAdapter {
 
-	@Override
-	public Object getChild(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	private static final class ViewHolder {
+		TextView textLabel;
+	}
+
+	private final List<CategoriesItem> itemList;
+	private final LayoutInflater inflater;
+
+	public CategoriesListAdapter(Context context, List<CategoriesItem> itemList) {
+		this.inflater = LayoutInflater.from(context);
+		this.itemList = itemList;
 	}
 
 	@Override
-	public long getChildId(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return 0;
+	public Object getChild(int groupPosition, int childPosition) {
+		return itemList.get(groupPosition).getChildItemList()
+				.get(childPosition);
 	}
 
 	@Override
-	public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
-			ViewGroup arg4) {
-		// TODO Auto-generated method stub
-		return null;
+	public long getChildId(int groupPosition, int childPosition) {
+		return childPosition;
 	}
 
 	@Override
-	public int getChildrenCount(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public View getChildView(int groupPosition, int childPosition,
+			boolean isLastChild, View convertView, final ViewGroup parent) {
+		View resultView = convertView;
+		ViewHolder holder;
+
+		if (resultView == null) {
+
+			resultView = inflater.inflate(android.R.layout.expandable_list_content, null);
+															
+			holder = new ViewHolder();
+			holder.textLabel = (TextView) resultView
+					.findViewById(android.R.id.title); // TODO change view id
+			resultView.setTag(holder);
+		} else {
+			holder = (ViewHolder) resultView.getTag();
+		}
+
+		final Category item = (Category) getChild(groupPosition, childPosition);
+
+		holder.textLabel.setText(item.toString());
+
+		return resultView;
 	}
 
 	@Override
-	public Object getGroup(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public int getChildrenCount(int groupPosition) {
+		return itemList.get(groupPosition).getChildItemList().size();
+	}
+
+	@Override
+	public Object getGroup(int groupPosition) {
+		return itemList.get(groupPosition);
 	}
 
 	@Override
 	public int getGroupCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return itemList.size();
 	}
 
 	@Override
 	public long getGroupId(int arg0) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public View getGroupView(int arg0, boolean arg1, View arg2, ViewGroup arg3) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public View getGroupView(int groupPosition, boolean isExpanded, View theConvertView, ViewGroup parent) {
+        View resultView = theConvertView;
+        ViewHolder holder;
+
+        if (resultView == null) {
+                resultView = inflater.inflate(android.R.layout.expandable_list_content, null); 
+                holder = new ViewHolder();
+                holder.textLabel = (TextView) resultView.findViewById(android.R.id.title);
+                resultView.setTag(holder);
+        } else {
+                holder = (ViewHolder) resultView.getTag();
+        }
+
+        final CategoriesItem item = (CategoriesItem) getGroup(groupPosition);
+
+        holder.textLabel.setText(item.toString());
+
+        return resultView;
+}
 
 	@Override
 	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
-	public boolean isChildSelectable(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return true;
 	}
 
 }
